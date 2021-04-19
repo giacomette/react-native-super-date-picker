c7534a68d3b1e992bf751c01444c9dc2b285b211
Roberto <roberto.giacomette@nuvem.net>
2021-04-19 19:53:42 -0300

    feat(ajustes): fixed type

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,11 @@
 /* eslint-disable react-native/no-inline-styles */
-import React, { useState, useCallback, useEffect, useRef } from 'react';
+import React, {
+  useState,
+  useCallback,
+  useEffect,
+  useRef,
+  useImperativeHandle,
+} from 'react';
 
 import { TouchableOpacity, View } from 'react-native';
 
@@ -16,168 +22,189 @@ const { width } = Dimensions.get('screen');
 interface DatePickerProps {
   startDate?: Date;
   endDate?: Date;
+  ref?: any;
   onChange?: (startDate: Date, endDate: Date) => void;
 }
 
-function DatePicker({ startDate, endDate, onChange }: DatePickerProps) {
-  const [initialDate, setInitialDate] = useState<Date>(new Date());
-  const [finishDate, setFinishDate] = useState<Date>(new Date());
-
-  const [openModal, setOpenModal] = useState(true);
-
-  const refCalendar = useRef<CalendarPicker>(null);
-
-  useEffect(() => {
-    if (endDate !== undefined) {
-      setFinishDate(endDate);
-    }
-  }, [endDate]);
-
-  useEffect(() => {
-    if (startDate !== undefined) {
-      setInitialDate(startDate);
-    }
-  }, [startDate]);
-
-  const handleDate = useCallback(
-    (date: Date, type: 'START_DATE' | 'END_DATE') => {
-      if (type === 'END_DATE') {
-        setFinishDate(date);
-      } else {
-        setInitialDate(date);
-      }
-    },
-    []
-  );
-
-  const handleOk = useCallback(() => {
-    if (typeof onChange === 'function') {
-      onChange(startDate as Date, finishDate);
-    }
-    setOpenModal(false);
-  }, [finishDate, onChange, startDate]);
-
-  const handlePrevMounth = useCallback(() => {
-    refCalendar.current?.handleOnPressPrevious();
-  }, [refCalendar]);
-
-  const handleNextMounth = useCallback(() => {
-    refCalendar.current?.handleOnPressNext();
-  }, [refCalendar]);
-
-  return (
-    <Modal
-      onDismiss={() => setOpenModal(false)}
-      isVisible={openModal}
-      animationIn="fadeIn"
-      onBackdropPress={() => setOpenModal(false)}
-    >
-      <Div bg={'white'} rounded={8} mx={16}>
-        <Div p={16} bg={'#11A557'} roundedTop={8}>
-          <Div>
-            <Text style={styles.title}>Selecione um período</Text>
-          </Div>
-          <Div mt={16}>
-            <Text color="white" fontWeight="bold" fontSize={24}>
-              {moment(initialDate).format('DD [de] MMMM')} -{' '}
-              {moment(finishDate).format('DD [de] MMMM')}
-            </Text>
-          </Div>
-        </Div>
-
-        <Div mt={8}>
-          <CalendarPicker
-            width={width - 48}
-            ref={refCalendar}
-            selectedStartDate={startDate}
-            selectedEndDate={endDate}
-            selectedDayColor={'#def0e8'}
-            onDateChange={(v: Moment, type: any) =>
-              handleDate(v.toDate(), type)
-            }
-            allowRangeSelection
-            allowBackwardRangeSelect
-            selectedRangeStartTextStyle={{
-              color: 'white',
-            }}
-            selectedRangeStartStyle={{
-              backgroundColor: '#11A557',
-            }}
-            selectedRangeEndTextStyle={{
-              color: 'white',
-            }}
-            selectedRangeEndStyle={{
-              backgroundColor: '#11A557',
-            }}
-            weekdays={['Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sab', 'Dom']}
-            months={[
-              'Janeiro',
-              'Fevereiro',
-              'Março',
-              'Abril',
-              'Maio',
-              'Junho',
-              'Julho',
-              'Agosto',
-              'Setembro',
-              'Outubro',
-              'Novembro',
-              'Dezembro',
-            ]}
-            previousComponent={
-              <View style={styles.previousWrapper}>
-                <TouchableOpacity
-                  activeOpacity={0.8}
-                  onPress={() => handlePrevMounth()}
-                >
-                  <Icon
-                    name="caretleft"
-                    fontFamily="AntDesign"
-                    fontSize={16}
-                    color={'#2C2C2C'}
-                  />
-                </TouchableOpacity>
-              </View>
-            }
-            nextComponent={
-              <View style={styles.nextWrapper}>
-                <TouchableOpacity
-                  activeOpacity={0.8}
-                  onPress={() => handleNextMounth()}
-                >
-                  <Icon name="ant-caretright" fontSize={16} color={'#2C2C2C'} />
-                </TouchableOpacity>
-              </View>
-            }
-          />
-        </Div>
-        <Div flexDir={'row'} justifyContent={'center'} p={16}>
-          <Div flex={1} mr={8}>
-            <Button
-              labelStyle={[{ color: '#2C2C2C' }, styles.buttonLabel]}
-              onPress={() => setOpenModal(false)}
-              testID="datepicker-cancel"
-              uppercase={false}
-            >
-              Cancelar
-            </Button>
-          </Div>
-          <Div flex={1} ml={8}>
-            <Button
-              mode="contained"
-              labelStyle={styles.buttonLabel}
-              onPress={() => handleOk()}
-              testID="datepicker-ok"
-              color={'#11A557'}
-            >
-              Ok
-            </Button>
-          </Div>
-        </Div>
-      </Div>
-    </Modal>
-  );
+export interface DatePickerRef {
+  close: () => void;
+  open: () => void;
 }
+
+const DatePicker = React.forwardRef<DatePickerRef, DatePickerProps>(
+  ({ startDate, endDate, onChange }, ref) => {
+    const [initialDate, setInitialDate] = useState<Date>(new Date());
+    const [finishDate, setFinishDate] = useState<Date>(new Date());
+
+    const [openModal, setOpenModal] = useState(false);
+
+    const refCalendar = useRef<CalendarPicker>(null);
+
+    useImperativeHandle(ref, () => ({
+      open() {
+        setOpenModal(true);
+      },
+      close() {
+        setOpenModal(false);
+      },
+    }));
+
+    useEffect(() => {
+      if (endDate !== undefined) {
+        setFinishDate(endDate);
+      }
+    }, [endDate]);
+
+    useEffect(() => {
+      if (startDate !== undefined) {
+        setInitialDate(startDate);
+      }
+    }, [startDate]);
+
+    const handleDate = useCallback(
+      (date: Date, type: 'START_DATE' | 'END_DATE') => {
+        if (type === 'END_DATE') {
+          setFinishDate(date);
+        } else {
+          setInitialDate(date);
+        }
+      },
+      []
+    );
+
+    const handleOk = useCallback(() => {
+      if (typeof onChange === 'function') {
+        onChange(startDate as Date, finishDate);
+      }
+      setOpenModal(false);
+    }, [finishDate, onChange, startDate]);
+
+    const handlePrevMounth = useCallback(() => {
+      refCalendar.current?.handleOnPressPrevious();
+    }, [refCalendar]);
+
+    const handleNextMounth = useCallback(() => {
+      refCalendar.current?.handleOnPressNext();
+    }, [refCalendar]);
+
+    return (
+      <Modal
+        onDismiss={() => setOpenModal(false)}
+        isVisible={openModal}
+        animationIn="fadeIn"
+        onBackdropPress={() => setOpenModal(false)}
+      >
+        <Div bg={'white'} rounded={8} mx={16}>
+          <Div p={16} bg={'#11A557'} roundedTop={8}>
+            <Div>
+              <Text style={styles.title}>Selecione um período</Text>
+            </Div>
+            <Div mt={16}>
+              <Text color="white" fontWeight="bold" fontSize={24}>
+                {moment(initialDate).format('DD [de] MMMM')} -{' '}
+                {moment(finishDate).format('DD [de] MMMM')}
+              </Text>
+            </Div>
+          </Div>
+
+          <Div mt={8}>
+            <CalendarPicker
+              width={width - 48}
+              ref={refCalendar}
+              selectedStartDate={startDate}
+              selectedEndDate={endDate}
+              selectedDayColor={'#def0e8'}
+              onDateChange={(v: Moment, type: any) =>
+                handleDate(v.toDate(), type)
+              }
+              allowRangeSelection
+              allowBackwardRangeSelect
+              selectedRangeStartTextStyle={{
+                color: 'white',
+              }}
+              selectedRangeStartStyle={{
+                backgroundColor: '#11A557',
+              }}
+              selectedRangeEndTextStyle={{
+                color: 'white',
+              }}
+              selectedRangeEndStyle={{
+                backgroundColor: '#11A557',
+              }}
+              weekdays={['Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sab', 'Dom']}
+              months={[
+                'Janeiro',
+                'Fevereiro',
+                'Março',
+                'Abril',
+                'Maio',
+                'Junho',
+                'Julho',
+                'Agosto',
+                'Setembro',
+                'Outubro',
+                'Novembro',
+                'Dezembro',
+              ]}
+              previousComponent={
+                <View style={styles.previousWrapper}>
+                  <TouchableOpacity
+                    activeOpacity={0.8}
+                    onPress={() => handlePrevMounth()}
+                  >
+                    <Icon
+                      name="caretleft"
+                      fontFamily="AntDesign"
+                      fontSize={16}
+                      color={'#2C2C2C'}
+                    />
+                  </TouchableOpacity>
+                </View>
+              }
+              nextComponent={
+                <View style={styles.nextWrapper}>
+                  <TouchableOpacity
+                    activeOpacity={0.8}
+                    onPress={() => handleNextMounth()}
+                  >
+                    <Icon
+                      name="ant-caretright"
+                      fontSize={16}
+                      color={'#2C2C2C'}
+                    />
+                  </TouchableOpacity>
+                </View>
+              }
+            />
+          </Div>
+          <Div flexDir={'row'} justifyContent={'center'} p={16}>
+            <Div flex={1} mr={8}>
+              <Button
+                labelStyle={[{ color: '#2C2C2C' }, styles.buttonLabel]}
+                onPress={() => setOpenModal(false)}
+                testID="datepicker-cancel"
+                uppercase={false}
+              >
+                Cancelar
+              </Button>
+            </Div>
+            <Div flex={1} ml={8}>
+              <Button
+                mode="contained"
+                labelStyle={styles.buttonLabel}
+                onPress={() => handleOk()}
+                testID="datepicker-ok"
+                color={'#11A557'}
+              >
+                Ok
+              </Button>
+            </Div>
+          </Div>
+        </Div>
+      </Modal>
+    );
+  }
+);
 
 const styles = StyleSheet.create({
   title: {
